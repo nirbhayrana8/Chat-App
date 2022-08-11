3209cb112fd52cbcc1de1284e6638cac72774661
Nirbhay <nirbhayrana8@gmail.com>
2022-08-11 23:01:43 +0530

    client

---
+++ client/src/components/NewContactModal.js
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react'
+import { Button, Form, Modal } from 'react-bootstrap'
+import { useContacts } from '../contexts/ContactsProvider';
+
+export default function NewContactModal({ closeModal }) {
+
+	const idRef = useRef();
+	const nameRef = useRef();
+	const { createContact } = useContacts();
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		createContact(idRef.current.value, nameRef.current.value)
+		closeModal();
+	}
+
+	return (
+		<>
+			<Modal.Header closeButton>Create Contact</Modal.Header>
+			<Modal.Body>
+				<Form onSubmit={handleSubmit}>
+					<Form.Group>
+						<Form.Label>Id</Form.Label>
+						<Form.Control type="text" ref={idRef} required></Form.Control>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Name</Form.Label>
+						<Form.Control type="text" ref={nameRef} required></Form.Control>
+					</Form.Group>
+					<Button className="mt-2" type="submit">Create contact</Button>
+				</Form>
+			</Modal.Body>
+		</>
+	)
+}
